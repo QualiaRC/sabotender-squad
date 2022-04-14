82988a25f3e00de0c5952e0466ffc595083168dd
Ryan Clark <clar1513@umn.edu>
2022-04-14 18:12:08 -0500

    Minor CSS changes

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.querySelector("#imageModal");
-    modal.close = function() {
-        this.setAttribute("noDisplay", '');
+var modal = document.querySelector("#imageModal");
+modal.close = function() {
+    this.setAttribute("nodisplay", '');
+}
+document.querySelector("#imageModal > .close").onclick = function() {
+    modal.close();
+};
+document.querySelectorAll('.imageContainer > div > img').forEach(function(element) {
+    element.onclick = function() {
+        modal.removeAttribute("nodisplay");
+        document.querySelector("#imageModal .modal-content").src = element.src;
+        document.querySelector("#imageModal .caption").textContent = element.getAttribute("caption") || "";
     }
-
-    document.querySelector("#imageModal > .close").onclick = function() {
-        modal.close();
-    };
-    document.querySelectorAll('.imageContainer > div > img').forEach(function(element) {
-        element.onclick = function() {
-            document.querySelector("#imageModal").removeAttribute("noDisplay");
-            document.querySelector("#imageModal .modal-content").src = element.src;
-            document.querySelector("#imageModal .caption").textContent = element.getAttribute("caption") || "";
-        }
-    });
-    document.onkeydown = function(event) {
-        event = event || window.event;
-        if(event.key == "Escape") {
-            if(!modal.hasAttribute("noDisplay")) {
-                modal.close();
-            }
-        }
-    };
-    document.querySelector("#year").textContent = new Date().getFullYear();
 });
+document.onkeydown = function(event) {
+    event = event || window.event;
+    if(event.key == "Escape" && !modal.hasAttribute("nodisplay")) {
+        modal.close();
+    }
+};
+document.querySelector("#year").textContent = new Date().getFullYear();
